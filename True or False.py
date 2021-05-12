7d71fe2fd34448d22b18cd4d410d52f032a9ec4f
ClvrName <84090630+ClvrName@users.noreply.github.com>
2021-05-12 09:36:02 -0700

    Add files via upload

---
+++ True or False.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+groot = tk.Tk()
+groot.title("True or False")
+groot.geometry("650x700")
+groot.configure(bg='red')
+
+my_tkVar = tk.IntVar()
+my_tkVar.set(2)
+
+
+L1 = tk.Label(groot, text="Question", font=('arial', 25))
+L1.grid(row=0, column=0)
+L1.pack()
+
+L2 = tk.Label(groot, text=input("Question goes here"), font=('arial', 20))
+L2.pack()
+
+my_radio_1 = tk.Radiobutton(groot)
+my_radio_1.config(text=input("write true answer here"), variable=my_tkVar, value=1, font=(50), height=2, width=5)
+my_radio_2 = tk.Radiobutton(groot)
+my_radio_2.config(text=input("write false answer here"), variable=my_tkVar, value=2, font=(50), height=2, width=5)
+
+my_radio_1.pack()
+my_radio_2.pack()
+
