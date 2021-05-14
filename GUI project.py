f34afb7b745f808011b16413cf4bb74f657ba5df
MasonPrice22 <84092478+MasonPrice22@users.noreply.github.com>
2021-05-14 14:09:20 -0700

    Add files via upload

--- GUI project.py
+++ GUI project.py
@@ -23,6 +23,12 @@ def doButton2():
 def doButton3():
     frame_6.tkraise()
 
+def doButton8():
+    frame_8.tkraise()
+
+def doButton9():
+    bottom_frame.tkraise()
+
 frame_3 = tk.Frame(root, bg="red")
 frame_3.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -77,23 +83,23 @@ frame_3.place(x=0, y=0, relwidth=1, relheight=1)
 TitleLabel = tk.Label(frame_3,text= "Create Your Own Quiz", relief='solid',borderwidth = 1, bg='deep sky blue', font=('Times New Roman', '24', 'bold'))
 TitleLabel.place(x=0, y=0, relwidth = 1, relheight = .08)
 
-L1 = tk.Label(frame_3, text = "Question:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L1.place(rely=.11,relx=.05,relheight=.05, anchor='w')
+L4 = tk.Label(frame_3, text = "Question:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L4.place(rely=.11,relx=.05,relheight=.05, anchor='w')
 
-E1 = tk.Entry(frame_3, width = 90)
-E1.place(rely=.15,relx=.05,relheight=.05, anchor='w')
+E3 = tk.Entry(frame_3, width = 90)
+E3.place(rely=.15,relx=.05,relheight=.05, anchor='w')
 
-L2 = tk.Label(frame_3, text = "Option A:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L2.place(rely=.20,relx=.05,relheight=.05, anchor='w')
+L5 = tk.Label(frame_3, text = "Option A:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L5.place(rely=.20,relx=.05,relheight=.05, anchor='w')
 
-L3 = tk.Label(frame_3, text = "Option B:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L3.place(rely=.35,relx=.05,relheight=.05, anchor='w')
+L6 = tk.Label(frame_3, text = "Option B:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L6.place(rely=.35,relx=.05,relheight=.05, anchor='w')
 
-L4 = tk.Label(frame_3, text = "Option C:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L4.place(rely=.50,relx=.05,relheight=.05, anchor='w')
+L7 = tk.Label(frame_3, text = "Option C:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L7.place(rely=.50,relx=.05,relheight=.05, anchor='w')
 
-L5 = tk.Label(frame_3, text = "Option D:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L5.place(rely=.65,relx=.05,relheight=.05, anchor='w')
+L8 = tk.Label(frame_3, text = "Option D:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L8.place(rely=.65,relx=.05,relheight=.05, anchor='w')
 
 
 my_checkbutton_1=tk.Checkbutton(frame_3,var=my_bool1,bg="indianred1", font=('Arial','16'))
@@ -104,20 +110,20 @@ my_checkbutton_4=tk.Checkbutton(frame_3,var=my_bool4,bg="indianred1", font=('Ari
 
 
 my_checkbutton_1.place(rely=.25,relx=.05,relheight=.05, anchor='w')
-E2 = tk.Entry(frame_3, width = 90)
-E2.place(rely=.25,relx=.10,relheight=.05, anchor='w')
+E4 = tk.Entry(frame_3, width = 90)
+E4.place(rely=.25,relx=.10,relheight=.05, anchor='w')
 
 my_checkbutton_2.place(rely=.40,relx=.05,relheight=.05, anchor='w')
-E3 = tk.Entry(frame_3, width = 90)
-E3.place(rely=.40,relx=.10,relheight=.05, anchor='w')
+E5 = tk.Entry(frame_3, width = 90)
+E5.place(rely=.40,relx=.10,relheight=.05, anchor='w')
 
 my_checkbutton_3.place(rely=.55,relx=.05,relheight=.05, anchor='w')
-E4 = tk.Entry(frame_3, width = 90)
-E4.place(rely=.55,relx=.10,relheight=.05, anchor='w')
+E6 = tk.Entry(frame_3, width = 90)
+E6.place(rely=.55,relx=.10,relheight=.05, anchor='w')
 
 my_checkbutton_4.place(rely=.70,relx=.05,relheight=.05, anchor='w')
-E5 = tk.Entry(frame_3, width = 90)
-E5.place(rely=.70,relx=.10,relheight=.05, anchor='w')
+E7 = tk.Entry(frame_3, width = 90)
+E7.place(rely=.70,relx=.10,relheight=.05, anchor='w')
 
 B3 = tk.Button(frame_3,text="Next", bg='limegreen', width=7,font=('Arial','16'), command = doButton2  )
 B3.place(rely=.85,relx=.8,relheight=.05, anchor='w')
@@ -139,11 +145,11 @@ my_tkVar.set(2)
 TitleLabel = tk.Label(frame_6,text= "True or False Question Appearance", relief='solid',borderwidth = 1, bg='deep sky blue', font=('Times New Roman', '24', 'bold'))
 TitleLabel.place(x=0, y=0, relwidth = 1, relheight = .08)
 
-L1 = tk.Label(frame_6, text = "Type Question:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
-L1.place(rely=.11,relx=.05,relheight=.05, anchor='w')
+L9 = tk.Label(frame_6, text = "Type Question:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L9.place(rely=.11,relx=.05,relheight=.05, anchor='w')
 
-E1 = tk.Entry(frame_6, width = 90)
-E1.place(rely=.15,relx=.05,relheight=.05, anchor='w')
+E8 = tk.Entry(frame_6, width = 90)
+E8.place(rely=.15,relx=.05,relheight=.05, anchor='w')
 
 my_radio_1 = tk.Radiobutton(frame_6,text=("True"), variable=my_tkVar,value=1, font=('Arial','16'),bg='indianred1')
 my_radio_1.place(rely = .30,relx= .08, relheight = .05, anchor = 'w')
@@ -155,6 +161,39 @@ my_radio_2.place(rely = .45,relx= .08, relheight = .05, anchor = 'w')
 B3 = tk.Button(frame_6,text="Next", bg='limegreen', width=7,font=('Arial','16'), command = doButton2  )
 B3.place(rely=.85,relx=.8,relheight=.05, anchor='w')
 
+#Frame 8
+
+bottom_frame = tk.Frame(root, bg="indianred1", padx=5, pady=5)
+bottom_frame.grid(row=1, column=0, padx=10, pady=10, sticky=(tk.NSEW))
+
+bottom_frame.columnconfigure(0, weight=1)
+bottom_frame.rowconfigure(0, weight=1)
+
+frame_8 = tk.Frame(bottom_frame, bg="indianred1")
+frame_8.place(x=0, y=0, relwidth=1, relheight=1)
+
+TitleLabel = tk.Label(frame_8,text= "Quiz Results", relief='solid',borderwidth = 1, bg='deep sky blue', font=('Times New Roman', '24', 'bold'))
+TitleLabel.place(x=0, y=0, relwidth = 1, relheight = .08)
+
+L10 = tk.Label(frame_8, text = "Score:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L10.place(rely=.11,relx=.01,relheight=.05, anchor='w')
+
+L11 = tk.Label(frame_8, text = "Question:",height = 2, width = 30,bg="indianred1", font=('Arial','16'))
+L11.place(rely=.16,relx=.01,relheight=.05, anchor='w')
+
+def func_two():
+    str_var1 = E1.get()
+    L1.configure(text = str_var1)
+    if str_var1 == int:
+        L11.place(rely=.11,relx=.05,relheight=.05, anchor='w')*str_var1
+
+B3 = tk.Button(frame_8,text="Show Questions",width=15,font=('Arial','16'), command = func_two  )
+B3.place(rely=.85,relx=.01,relheight=.05, anchor='w')
+
+B3 = tk.Button(frame_8,text="Reset", bg='limegreen', width=7,font=('Arial','16'), command = doButton9  )
+B3.place(rely=.85,relx=.8,relheight=.05, anchor='w')
+
+    
 
 
 root.mainloop()
