e2c273db64b097bb41135dbc6f5c85c2c1afdf20
ClvrName <84090630+ClvrName@users.noreply.github.com>
2021-05-13 21:07:41 -0700

    Update True or False.py

--- True or False.py
+++ True or False.py
@@ -11,11 +11,9 @@ my_tkVar.set(2)
 
 L1 = tk.Label(groot, text="Question", font=('arial', 25))
 L1.grid(row = 0, column = 0, pady = 2)
-#L1.pack()
 
 L2 = tk.Label(groot, text=input("Question goes here"), font=('arial', 20))
 L2.grid(row = 1, column = 0, pady = 2)
-#L2.pack()
 
 my_radio_1 = tk.Radiobutton(groot)
 my_radio_1.config(text=input("write true answer here"), variable=my_tkVar, value=1, font=(50), height=2, width=5)
